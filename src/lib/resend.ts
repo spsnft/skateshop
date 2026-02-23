d53e10fbf6570a50d3a1cc41c6185622105590f7
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-24 01:33:02 +0700

    Update resend.ts

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,7 @@
 import { env } from "@/env.js"
 import { Resend } from "resend"
 
-export const resend = new Resend(env.RESEND_API_KEY)
+// Создаем объект только если ключ существует, иначе оставляем null
+export const resend = env.RESEND_API_KEY 
+  ? new Resend(env.RESEND_API_KEY) 
+  : null
