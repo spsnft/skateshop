81f601e7582b77a5c77178a38569bf3d37b29206
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-24 20:05:21 +0700

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { env } from "@/env.js"
-import type { User } from "@clerk/nextjs/server"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -7,84 +5,29 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
-}
-
 export function formatPrice(
   price: number | string,
-  opts: Intl.NumberFormatOptions = {}
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "THB"
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
 ) {
+  const { currency = "THB", notation = "standard" } = options
+
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: opts.currency ?? "USD",
-    notation: opts.notation ?? "compact",
-    ...opts,
+    currency,
+    notation,
+    maximumFractionDigits: 0,
   }).format(Number(price))
 }
 
-export function formatNumber(
-  number: number | string,
-  opts: Intl.NumberFormatOptions = {}
-) {
-  return new Intl.NumberFormat("en-US", {
-    style: opts.style ?? "decimal",
-    notation: opts.notation ?? "standard",
-    minimumFractionDigits: opts.minimumFractionDigits ?? 0,
-    maximumFractionDigits: opts.maximumFractionDigits ?? 2,
-    ...opts,
-  }).format(Number(number))
-}
-
-export function formatDate(
-  date: Date | string | number,
-  opts: Intl.DateTimeFormatOptions = {}
-) {
+export function formatDate(date: Date | string | number) {
   return new Intl.DateTimeFormat("en-US", {
-    month: opts.month ?? "long",
-    day: opts.day ?? "numeric",
-    year: opts.year ?? "numeric",
-    ...opts,
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   }).format(new Date(date))
-}
-
-export function formatBytes(
-  bytes: number,
-  decimals = 0,
-  sizeType: "accurate" | "normal" = "normal"
-) {
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
-  const accurateSizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"]
-  if (bytes === 0) return "0 Byte"
-  const i = Math.floor(Math.log(bytes) / Math.log(1024))
-  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
-    sizeType === "accurate"
-      ? (accurateSizes[i] ?? "Bytest")
-      : (sizes[i] ?? "Bytes")
-  }`
-}
-
-export function formatId(id: string) {
-  return `#${id.toString().padStart(4, "0")}`
-}
-
-export function slugify(str: string) {
-  return str
-    .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[^\w-]+/g, "")
-    .replace(/--+/g, "-")
-}
-
-export function unslugify(str: string) {
-  return str.replace(/-/g, " ")
-}
-
-export function toTitleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
-  )
 }
 
 export function toSentenceCase(str: string) {
@@ -93,20 +36,6 @@ export function toSentenceCase(str: string) {
     .replace(/^./, (str) => str.toUpperCase())
 }
 
-export function truncate(str: string, length: number) {
-  return str.length > length ? `${str.substring(0, length)}...` : str
-}
-
-export function getUserEmail(user: User | null) {
-  const email =
-    user?.emailAddresses?.find((e) => e.id === user.primaryEmailAddressId)
-      ?.emailAddress ?? ""
-
-  return email
-}
-
-export function isMacOs() {
-  if (typeof window === "undefined") return false
-
-  return window.navigator.userAgent.includes("Mac")
+export function idGenerator() {
+  return Math.random().toString(36).substr(2, 9)
 }
