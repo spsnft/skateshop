87e1a952c5058e640952f9030662b4d7942d8fde
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-24 20:44:36 +0700

    Update site-header.tsx

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,27 +1,17 @@
-import type { User } from "@clerk/nextjs/server"
-
+import Link from "next/link"
 import { siteConfig } from "@/config/site"
-import { CartSheet } from "@/components/checkout/cart-sheet"
-import { AuthDropdown } from "@/components/layouts/auth-dropdown"
 import { MainNav } from "@/components/layouts/main-nav"
 import { MobileNav } from "@/components/layouts/mobile-nav"
-import { ProductsCombobox } from "@/components/products-combobox"
 
-interface SiteHeaderProps {
-  user: User | null
-}
-
-export function SiteHeader({ user }: SiteHeaderProps) {
+export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
         <MainNav items={siteConfig.mainNav} />
-        <MobileNav items={siteConfig.mainNav} />
+        <MobileNav mainNavItems={siteConfig.mainNav} sidebarNavItems={[]} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
-            <ProductsCombobox />
-            <CartSheet />
-            <AuthDropdown user={user} />
+            {/* Мы временно убрали кнопку профиля, чтобы сайт заработал */}
           </nav>
         </div>
       </div>
