597bed16bf358d9c3ef35b94a008b58ed4fc79a1
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-24 19:00:53 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-import { withContentlayer } from "next-contentlayer"
-
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js")
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
@@ -17,9 +9,9 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  // Already doing linting and typechecking as separate tasks in CI
+  // Игнорируем ошибки линтинга и типов при сборке для успешного деплоя
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 }
 
-export default withContentlayer(nextConfig)
+export default nextConfig
