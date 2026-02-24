089361bd46ede42dbd2da95385a25320bd68a346
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-24 20:49:52 +0700

    Update site-header.tsx

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { MainNav } from "@/components/layouts/main-nav"
-import { MobileNav } from "@/components/layouts/mobile-nav"
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
         <MainNav items={siteConfig.mainNav} />
-        <MobileNav mainNavItems={siteConfig.mainNav} sidebarNavItems={[]} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
-            {/* Мы временно убрали кнопку профиля, чтобы сайт заработал */}
+            {/* Кнопки профиля и корзины пока скрыты для стабильности */}
           </nav>
         </div>
       </div>
