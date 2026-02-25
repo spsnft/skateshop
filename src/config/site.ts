e27f832eac25b82c5cdbc101140a49a6aa316780
spsnft <87615686+spsnft@users.noreply.github.com>
2026-02-25 23:21:01 +0700

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,120 +1,42 @@
 import type { FooterItem, MainNavItem } from "@/types"
-
 import { productConfig } from "@/config/product"
 import { slugify } from "@/lib/utils"
 
 export type SiteConfig = typeof siteConfig
 
 const links = {
-  x: "https://twitter.com/sadmann17",
-  github: "https://github.com/sadmann7/skateshop",
-  githubAccount: "https://github.com/sadmann7",
-  discord: "https://discord.com/users/sadmann7",
-  calDotCom: "https://cal.com/sadmann7",
+  // Твой ник прописан
+  telegram: "https://t.me/spacenft", 
 }
 
 export const siteConfig = {
-  name: "Skateshop",
+  name: "bnd",
   description:
-    "An open source e-commerce skateshop build with everything new in Next.js.",
-  url: "https://skateshop.sadmn.com",
-  ogImage: "https://skateshop.sadmn.com/opengraph-image.png",
+    "Premium Phuket Delivery Service. Top-tier selection delivered to your door.",
+  url: "https://app.bnd.delivery", 
+  ogImage: "https://app.bnd.delivery/og-image.png",
   links,
   mainNav: [
     {
       title: "Lobby",
       items: [
         {
-          title: "Products",
-          href: "/products",
-          description: "All the products we have to offer.",
-          items: [],
-        },
-        {
-          title: "Build a Board",
-          href: "/build-a-board",
-          description: "Build your own custom skateboard.",
-          items: [],
-        },
-        {
-          title: "Blog",
-          href: "/blog",
-          description: "Read our latest blog posts.",
+          title: "Inventory",
+          href: "/",
+          description: "Our current selection of premium products.",
           items: [],
         },
       ],
     },
-    ...productConfig.categories.map((category) => ({
-      title: category.name,
-      items: [
-        {
-          title: "All",
-          href: `/categories/${slugify(category.name)}`,
-          description: `All ${category.name}.`,
-          items: [],
-        },
-        ...category.subcategories.map((subcategory) => ({
-          title: subcategory.name,
-          href: `/categories/${slugify(category.name)}/${slugify(subcategory.name)}`,
-          description: subcategory.description,
-          items: [],
-        })),
-      ],
-    })),
   ] satisfies MainNavItem[],
   footerNav: [
     {
-      title: "Credits",
+      title: "Support",
       items: [
         {
-          title: "OneStopShop",
-          href: "https://onestopshop.jackblatch.com",
+          title: "Contact Telegram",
+          href: links.telegram,
           external: true,
-        },
-        {
-          title: "Acme Corp",
-          href: "https://acme-corp.jumr.dev",
-          external: true,
-        },
-        {
-          title: "craft.mxkaske.dev",
-          href: "https://craft.mxkaske.dev",
-          external: true,
-        },
-        {
-          title: "Taxonomy",
-          href: "https://tx.shadcn.com/",
-          external: true,
-        },
-        {
-          title: "shadcn/ui",
-          href: "https://ui.shadcn.com",
-          external: true,
-        },
-      ],
-    },
-    {
-      title: "Help",
-      items: [
-        {
-          title: "About",
-          href: "/about",
-          external: false,
-        },
-        {
-          title: "Contact",
-          href: "/contact",
-          external: false,
-        },
-        {
-          title: "Terms",
-          href: "/terms",
-          external: false,
-        },
-        {
-          title: "Privacy",
-          href: "/privacy",
-          external: false,
         },
       ],
     },
@@ -122,48 +44,8 @@ export const siteConfig = {
       title: "Social",
       items: [
         {
-          title: "X",
-          href: links.x,
-          external: true,
-        },
-        {
-          title: "GitHub",
-          href: links.githubAccount,
-          external: true,
-        },
-        {
-          title: "Discord",
-          href: links.discord,
-          external: true,
-        },
-        {
-          title: "cal.com",
-          href: links.calDotCom,
-          external: true,
-        },
-      ],
-    },
-    {
-      title: "Lofi",
-      items: [
-        {
-          title: "beats to study to",
-          href: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
-          external: true,
-        },
-        {
-          title: "beats to chill to",
-          href: "https://www.youtube.com/watch?v=rUxyKA_-grg",
-          external: true,
-        },
-        {
-          title: "a fresh start",
-          href: "https://www.youtube.com/watch?v=rwionZbOryo",
-          external: true,
-        },
-        {
-          title: "coffee to go",
-          href: "https://www.youtube.com/watch?v=2gliGzb2_1I",
+          title: "Telegram",
+          href: links.telegram,
           external: true,
         },
       ],
