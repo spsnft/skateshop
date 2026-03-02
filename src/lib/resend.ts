6190fabe8266a6351b4a61266a7107878f2349a8
spsnft <87615686+spsnft@users.noreply.github.com>
2026-03-02 20:54:09 +0700

    Delete src/lib/resend.ts

--- src/lib/resend.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "@/env.js"
-import { Resend } from "resend"
-
-// Создаем объект только если ключ существует, иначе оставляем null
-export const resend = env.RESEND_API_KEY 
-  ? new Resend(env.RESEND_API_KEY) 
-  : null
